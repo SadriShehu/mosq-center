b209f2616eb9681acb35a26dd164017addcb0b38
Ylli Guri <ylli.guri@suchagents.at>
2023-10-12 13:32:29 +0200

    MOSQ-001: implement service and repository functions

--- internal/repository/families.go
+++ internal/repository/families.go
@@ -1,1 +1,13 @@
 package repository
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type familiesRepository struct {
+	CDB *mongo.Client
+}
+
+func NewFamiliesRepository(CDB *mongo.Client) *familiesRepository {
+	return &familiesRepository{
+		CDB: CDB,
+	}
+}
--- internal/services/families.go
+++ internal/services/families.go
@@ -1,1 +1,16 @@
 package services
+
+import (
+	"github.com/sadrishehu/mosq-center/internal/models"
+)
+
+type familiesService struct {
+	FamiliesRepository models.FamiliesRepository
+}
+
+func NewFamiliesRepository(FamiliesRepository models.FamiliesRepository) *familiesService {
+	return &familiesService{
+		FamiliesRepository: FamiliesRepository,
+	}
+}
+
