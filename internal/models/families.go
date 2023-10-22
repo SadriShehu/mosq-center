bbed426959c3e0f313cf8ababa1ba31db3b96f3c
sadrishehu <sadrishehu@gmail.com>
2023-10-22 21:34:14 +0200

    MOSQ-008: implement families and payments data structures

--- internal/models/families.go
+++ internal/models/families.go
@@ -1,1 +1,61 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Families struct {
+	ID              string `json:"id"`
+	Name            string `json:"name"`
+	Middlename      string `json:"middle_name"`
+	Surname         string `json:"surname"`
+	Members         int    `json:"members"`
+	NeighbourhoodID string `json:"neighbourhood_id"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
+	DeletedAt       string `json:"deleted_at"`
+}
+
+func (f *Families) Hydrate(req *FamiliesRequest) {
+	f.Name = req.Name
+	f.Middlename = req.Middlename
+	f.Surname = req.Surname
+	f.Members = req.Members
+	f.NeighbourhoodID = req.NeighbourhoodID
+
+	f.ID = uuid.New().String()
+	f.CreatedAt = time.Now().UTC().Format(time.RFC3339)
+	f.UpdatedAt = time.Now().UTC().Format(time.RFC3339)
+}
+
+type FamiliesRequest struct {
+	Name            string `json:"name" validate:"required"`
+	Middlename      string `json:"middle_name"`
+	Surname         string `json:"surname" validate:"required"`
+	Members         int    `json:"members" validate:"required"`
+	NeighbourhoodID string `json:"neighbourhood_id" validate:"required"`
+}
+
+type FamiliesResponse struct {
+	ID              string `json:"id"`
+	Name            string `json:"name"`
+	Middlename      string `json:"middle_name"`
+	Surname         string `json:"surname"`
+	Members         int    `json:"members"`
+	NeighbourhoodID string `json:"neighbourhood_id"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
+}
+
+func (f *FamiliesResponse) MapResponse(family *Families) {
+	f.ID = family.ID
+	f.Name = family.Name
+	f.Middlename = family.Middlename
+	f.Surname = family.Surname
+	f.Members = family.Members
+	f.NeighbourhoodID = family.NeighbourhoodID
+	f.CreatedAt = family.CreatedAt
+	f.UpdatedAt = family.UpdatedAt
+}
--- internal/models/payments.go
+++ internal/models/payments.go
@@ -1,1 +1,43 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Payments struct {
+	ID              string `json:"id"`
+	FamilyID        string `json:"family_id"`
+	Amount          int    `json:"amount"`
+	NeighbourhoodID string `json:"neighbourhood_id"`
+	Year            int    `json:"year"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
+	DeletedAt       string `json:"deleted_at"`
+}
+
+func (p *Payments) Hydrate(req *PaymentsRequest) {
+	p.FamilyID = req.FamilyID
+	p.Amount = req.Amount
+	p.NeighbourhoodID = req.NeighbourhoodID
+
+	p.ID = uuid.New().String()
+	p.CreatedAt = time.Now().UTC().Format(time.RFC3339)
+	p.UpdatedAt = time.Now().UTC().Format(time.RFC3339)
+}
+
+type PaymentsRequest struct {
+	FamilyID        string `json:"family_id" validate:"required"`
+	Amount          int    `json:"amount" validate:"required"`
+	NeighbourhoodID string `json:"neighbourhood_id" validate:"required"`
+}
+
+type PaymentsResponse struct {
+	ID              string `json:"id"`
+	FamilyID        string `json:"family_id"`
+	Amount          int    `json:"amount"`
+	NeighbourhoodID string `json:"neighbourhood_id"`
+	CreatedAt       string `json:"created_at"`
+	UpdatedAt       string `json:"updated_at"`
+}
