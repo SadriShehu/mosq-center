4317b76568ecc3c9b401759cddc86d8828879914
sadrishehu <sadrishehu@gmail.com>
2023-11-25 13:22:50 +0100

    MOSQ-023: init terraform

---
+++ infrastructure/gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-fourth-walker-185309"
+    prefix = "terraform/state/mosq-center"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">=3.5.0"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">=3.5.0"
+    }
+  }
+
+  required_version = ">= 0.12.0"
+}
