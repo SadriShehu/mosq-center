6f0ce4d01e391eaaf12bc455519047bd38384af4
sadrishehu <sadrishehu@gmail.com>
2023-12-22 13:44:03 +0100

    MOSQ-033: Refactor server initialization and configuration

--- cmd/main.go
+++ cmd/main.go
@@ -4,25 +4,21 @@ import (
 	"context"
 	"encoding/gob"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/gorilla/sessions"
-	"github.com/rs/cors"
 	"github.com/sadrishehu/mosq-center/config"
-	"github.com/sadrishehu/mosq-center/internal/db"
-	"github.com/sadrishehu/mosq-center/internal/handlers"
-	"github.com/sadrishehu/mosq-center/internal/integration/auth0"
-	"github.com/sadrishehu/mosq-center/internal/integration/prayers"
-	"github.com/sadrishehu/mosq-center/internal/repository"
-	"github.com/sadrishehu/mosq-center/internal/services"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/sadrishehu/mosq-center/internal/server"
 )
+
+type Engine interface {
+	Bootstrap()
+	Run()
+	Shutdown(context.Context)
+	CloseDBConn(context.Context)
+}
 
 func init() {
 	gob.Register(map[string]interface{}{})
@@ -32,122 +28,21 @@ func main() {
 	c := config.New()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer func() {
-		cancel()
-	}()
+	defer cancel()
 
-	dbc, err := db.New(ctx, c.MongoDBURI+c.MongoUserCertPath)
-	if err != nil {
-		panic(err)
-	}
-	defer dbc.Disconnect(ctx)
+	var engine Engine = server.NewEngine(ctx, c)
+	engine.Bootstrap()
+	defer engine.CloseDBConn(ctx)
 
-	var auth *auth0.Authenticator
-	if c.Auth.Enable {
-		auth, err = auth0.New(c)
-		if err != nil {
-			log.Fatalf("Failed to initialize the authenticator: %v", err)
-		}
-	}
-
-	prayersClient := prayers.NewPrayersClient(http.DefaultClient)
-	prayersClient.SetTune(&prayers.Tune{
-		Imsak:    c.TunePrayers.Imsak,
-		Fajr:     c.TunePrayers.Fajr,
-		Sunrise:  c.TunePrayers.Sunrise,
-		Dhuhr:    c.TunePrayers.Dhuhr,
-		Asr:      c.TunePrayers.Asr,
-		Sunset:   c.TunePrayers.Sunset,
-		Maghrib:  c.TunePrayers.Maghrib,
-		Isha:     c.TunePrayers.Isha,
-		Midnight: c.TunePrayers.Midnight,
-	})
-
-	r := chi.NewRouter()
-
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.Logger)
-	ch := cors.New(cors.Options{
-		AllowedOrigins:     []string{"*"},
-		AllowedHeaders:     []string{"*"},
-		AllowedMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"},
-		AllowCredentials:   true,
-		Debug:              true,
-		OptionsPassthrough: false,
-	})
-	r.Use(ch.Handler)
-
-	services := newService(c, r, dbc, auth, prayersClient)
-	services.bootstrap()
-
-	srv := &http.Server{
-		Addr:    c.Port,
-		Handler: r,
-	}
-
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("failed to serve: %v\n", err)
-		}
-	}()
-
-	log.Printf("server is starting at %s...", srv.Addr)
+	log.Printf("server is starting at %s...", c.Port)
+	go engine.Run()
 
 	// Receive signal to shutdown the server.
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	log.Printf("signal %d received, shutting down gracefully...", <-quit)
-
-	// Gracefully shutdown the server
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("could not gracefully shutdown the server: %v\n", err)
-	}
+	engine.Shutdown(ctx)
 
 	log.Println("finished graceful shutdown")
-}
-
-type service struct {
-	config     *config.Config
-	router     *chi.Mux
-	nosql      *mongo.Client
-	auth0      *auth0.Authenticator
-	prayersAPI *prayers.PrayersClient
-}
-
-func newService(
-	c *config.Config,
-	r *chi.Mux,
-	nosql *mongo.Client,
-	auth0 *auth0.Authenticator,
-	prayersAPI *prayers.PrayersClient,
-) *service {
-	return &service{
-		config:     c,
-		router:     r,
-		nosql:      nosql,
-		auth0:      auth0,
-		prayersAPI: prayersAPI,
-	}
-}
-
-func (s *service) bootstrap() {
-	// Repository injection
-	pr := repository.NewPaymentsRepository(s.nosql)
-	fr := repository.NewFamiliesRepository(s.nosql)
-	nr := repository.NewNeighbourhoodsRepository(s.nosql)
-
-	// Service injection
-	ps := services.NewPaymentsService(pr)
-	fs := services.NewFamiliesService(fr)
-	ns := services.NewNeighbourhoodsRepository(nr)
-	is := services.NewInvoicesService(pr, nr)
-	prs := services.NewPrayersService(s.prayersAPI)
-
-	ss := sessions.NewCookieStore([]byte(s.config.Auth.SessionsSecret))
-	// Handler injection
-	h := handlers.New(s.router, s.auth0, ps, fs, ns, is, prs, ss, s.config.Auth)
-
-	h.RegisterRoutesV1()
-	h.RegisterTemplates()
 }
---
+++ internal/server/engine.go
@@ -0,0 +1,148 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/gorilla/sessions"
+	"github.com/rs/cors"
+	"github.com/sadrishehu/mosq-center/config"
+	"github.com/sadrishehu/mosq-center/internal/db"
+	"github.com/sadrishehu/mosq-center/internal/handlers"
+	"github.com/sadrishehu/mosq-center/internal/integration/auth0"
+	"github.com/sadrishehu/mosq-center/internal/integration/prayers"
+	"github.com/sadrishehu/mosq-center/internal/repository"
+	"github.com/sadrishehu/mosq-center/internal/services"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type engine struct {
+	config     *config.Config
+	router     *chi.Mux
+	nosql      *mongo.Client
+	auth0      *auth0.Authenticator
+	prayersAPI *prayers.PrayersClient
+	server     *http.Server
+}
+
+func NewEngine(
+	ctx context.Context,
+	c *config.Config,
+) *engine {
+	r := router()
+	return &engine{
+		config:     c,
+		router:     r,
+		nosql:      dbc(ctx, c),
+		auth0:      auth(c.Auth),
+		prayersAPI: prayersClient(c.TunePrayers),
+		server: &http.Server{
+			Addr:    c.Port,
+			Handler: r,
+		},
+	}
+}
+
+func dbc(ctx context.Context, c *config.Config) *mongo.Client {
+	dbc, err := db.New(ctx, c.MongoDBURI+c.MongoUserCertPath)
+	if err != nil {
+		log.Fatalf("failed to connect to the database: %v\n", err)
+	}
+
+	return dbc
+}
+
+func auth(c *config.Auth0Config) *auth0.Authenticator {
+	var (
+		auth *auth0.Authenticator
+		err  error
+	)
+
+	if c.Enable {
+		auth, err = auth0.New(c)
+		if err != nil {
+			log.Fatalf("Failed to initialize the authenticator: %v", err)
+		}
+	}
+
+	return auth
+}
+
+func router() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+	ch := cors.New(cors.Options{
+		AllowedOrigins:     []string{"*"},
+		AllowedHeaders:     []string{"*"},
+		AllowedMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"},
+		AllowCredentials:   true,
+		Debug:              true,
+		OptionsPassthrough: false,
+	})
+	r.Use(ch.Handler)
+
+	return r
+}
+
+func prayersClient(c *config.TunePrayers) *prayers.PrayersClient {
+	prayersClient := prayers.NewPrayersClient(http.DefaultClient)
+	prayersClient.SetTune(&prayers.Tune{
+		Imsak:    c.Imsak,
+		Fajr:     c.Fajr,
+		Sunrise:  c.Sunrise,
+		Dhuhr:    c.Dhuhr,
+		Asr:      c.Asr,
+		Sunset:   c.Sunset,
+		Maghrib:  c.Maghrib,
+		Isha:     c.Isha,
+		Midnight: c.Midnight,
+	})
+
+	return prayersClient
+}
+
+func (s *engine) Bootstrap() {
+	// Repository injection
+	pr := repository.NewPaymentsRepository(s.nosql)
+	fr := repository.NewFamiliesRepository(s.nosql)
+	nr := repository.NewNeighbourhoodsRepository(s.nosql)
+
+	// Service injection
+	ps := services.NewPaymentsService(pr)
+	fs := services.NewFamiliesService(fr)
+	ns := services.NewNeighbourhoodsRepository(nr)
+	is := services.NewInvoicesService(pr, nr)
+	prs := services.NewPrayersService(s.prayersAPI)
+
+	// Store injection
+	ss := sessions.NewCookieStore([]byte(s.config.Auth.SessionsSecret))
+
+	// Handler injection
+	h := handlers.New(s.router, s.auth0, ps, fs, ns, is, prs, ss, s.config.Auth)
+
+	h.RegisterRoutesV1()
+	h.RegisterTemplates()
+}
+
+func (s *engine) Run() {
+	if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("failed to serve: %v\n", err)
+	}
+}
+
+func (s *engine) Shutdown(ctx context.Context) {
+	if err := s.server.Shutdown(ctx); err != nil {
+		log.Fatalf("could not gracefully shutdown the server: %v\n", err)
+	}
+}
+
+func (s *engine) CloseDBConn(ctx context.Context) {
+	if err := s.nosql.Disconnect(ctx); err != nil {
+		log.Fatalf("could not close the database connection: %v\n", err)
+	}
+}
